a3db30ed9c7511f7edf1309366fa3f568226ca5a
Sean Oughton <seanoughton@gmail.com>
2018-01-04 22:06:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 def display_board(board)
   puts "  #{} | #{}  |  #{} "
   puts "-----------"
-  puts "   |   |   "
+  puts "  #{} | #{}  |  #{} "
   puts "-----------"
-  puts "   |   |   "
+  puts "  #{} | #{}  |  #{} "
 end
